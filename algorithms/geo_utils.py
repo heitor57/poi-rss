5cf281b45ea179ba960951c44141d221ac5d39e8
heitor57 <heitorwerneck@hotmail.com>
2019-08-30 17:42:37 -0300

    mercator distance

--- algorithms/geo_utils.py
+++ algorithms/geo_utils.py
@@ -1,3 +1,5 @@
+import numpy as np
+import math
 def haversine(lat1, lon1, lat2, lon2):
     # deg to rad
     lon1, lat1, lon2, lat2 = map(np.radians, [lon1, lat1, lon2, lat2])
@@ -7,3 +9,17 @@ def haversine(lat1, lon1, lat2, lon2):
     c = 2*np.arcsin(np.sqrt(a))
     r = 6371 # km
     return c*r
+
+VERY_SMALL_DEGREE = 0.00001
+def mercator(lat1,lon1,lat2,lon2):
+    if abs(lat1 - lat2) < VERY_SMALL_DEGREE and abs(lon1 - lon2) < VERY_SMALL_DEGREE:
+        return 0.0
+    
+    theta = lon1 - lon2
+    dist = math.sin(math.radians(lat1)) * math.sin(math.radians(lat2)) + math.cos(math.radians(lat1)) * math.cos(math.radians(lat2)) * math.cos(math.radians(theta))
+    dist = math.acos(dist)
+    dist = math.degrees(dist)
+    dist = dist * 60 * 1.1515
+    
+    dist = dist * 1.609344
+    return dist
