b07410d265fb584cf85ad65aa1104fce43fe4071
heitor57 <heitorwerneck@hotmail.com>
2019-09-06 17:10:46 -0300

    training and test and mostpopular equal to mymedialite mostpopular

--- algorithms/recommenders.py
+++ algorithms/recommenders.py
@@ -2,5 +2,9 @@ import numpy as np
 import pandas as pd
 
 ## Mostpopular rank by the top-k most visited pois by distinct users
-def mostpopular(df_review,k):
-    return df_review[['business_id','user_id']].groupby('business_id').count().sort_values('user_id',ascending=0).reset_index(level=0).rename(columns={"user_id":"score"}).head(k)
+def mostpopular(df_checkin,k,df_user_checkin,num_users,list_user_pois):
+    rec_list=df_checkin[['business_id','user_id']].groupby('business_id').nunique()[['user_id']].sort_values('user_id',ascending=0).reset_index(level=0).rename(columns={"user_id":"score"})
+    rec_list['score']=rec_list['score']/num_users
+    df_user_checkin['business_id']
+    rec_list=rec_list[rec_list.apply(lambda row: False if row['business_id'] in list_user_pois else True,axis=1)]
+    return rec_list
