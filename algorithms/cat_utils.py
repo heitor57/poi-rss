ffba06bfef6f8ee52f80185d32ec01d2c407b820
heitor57 <heitorwerneck@hotmail.com>
2019-10-09 11:07:14 -0300

    export with label category name

--- algorithms/cat_utils.py
+++ algorithms/cat_utils.py
@@ -1,10 +1,10 @@
 def get_most_detailed_categories(categories,dict_alias_title,dict_alias_depth):
     max_height=0
     for category in categories:
-        max_height = max(dict_alias_depth[dict_alias_title[category]],max_height)
+        max_height = max(dict_alias_depth[category],max_height)
     new_categories=list()
     for category in categories:
-        height=dict_alias_depth[dict_alias_title[category]]
+        height=dict_alias_depth[category]
         if(height == max_height):
             new_categories.append(category)
     return new_categories
