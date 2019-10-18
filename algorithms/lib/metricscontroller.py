674c1661789d6f2d6a91bac604590d859b2dbe77
heitor57 <heitorwerneck@hotmail.com>
2019-10-18 17:42:08 -0300

    default values

--- algorithms/lib/metricscontroller.py
+++ algorithms/lib/metricscontroller.py
@@ -1,12 +1,13 @@
 import numpy as np
 
 class MetricsController:
-    def __init__(self,metrics,algorithm,metrics_path):
+    def __init__(self,metrics,algorithm="generic",metrics_path="~/recsys_metrics",k=[10]):
         self.metrics={}
         for i in metrics:
             self.metrics[i]=[]
         self.algorithm=algorithm
         self.metrics_path=metrics_path
+        self.k=k
     def append_data(self,data):
         c=0
         for i in self.metrics.keys():
