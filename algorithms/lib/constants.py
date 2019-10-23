a1cb334ae969341522afb03fc874b26e4e5d07d9
heitor57 <heitorwerneck@hotmail.com>
2019-10-23 09:02:38 -0300

    constants with only variables, no functions

--- algorithms/lib/constants.py
+++ algorithms/lib/constants.py
@@ -1,43 +1,14 @@
 class experiment_constants:
-    _CITY="madison" # city source of checkins
-    @classmethod
-    def get_city(self):
-        return self._CITY
+    CITY="madison" # city source of checkins
+
 class geocat_constants:
-    _NEIGHBOR_DISTANCE = 0.5#km
-    _N = 80# temp list size
-    _K = 20# final list size
-    _VERY_SMALL_VALUE = -100 # used for objective function
-    _DIV_GEO_CAT_WEIGHT = 0.5 # beta,this is here because of the work to be done on parameter customization for each user
-    _DIV_WEIGHT=0.75 # lambda, geo vs cat_DIV_WEIGHT = 0.75 # lambda, geo vs cat
-
-    @classmethod
-    def get_neighbor_distance(self):
-        return self._NEIGHBOR_DISTANCE
-
-    @classmethod
-    def get_n(self):
-        return self._N
-
-    @classmethod
-    def get_k(self):
-        return self._K
-
-    @classmethod
-    def get_very_small_value(self):
-        return self._VERY_SMALL_VALUE
-
-    @classmethod
-    def get_div_geo_cat_weight(self):
-        return self._DIV_GEO_CAT_WEIGHT
-
-    @classmethod
-    def get_div_weight(self):
-        return self._DIV_WEIGHT
+    NEIGHBOR_DISTANCE = 0.5#km
+    N = 80# temp list size
+    K = 20# final list size
+    VERY_SMALL_VALUE = -100 # used for objective function
+    DIV_GEO_CAT_WEIGHT = 0.5 # beta,this is here because of the work to be done on parameter customization for each user
+    DIV_WEIGHT=0.75 # lambda, geo vs cat_DIV_WEIGHT = 0.75 # lambda, geo vs cat
 
 
 class usg_constants:
-    _eta=0.05
-    @classmethod
-    def get_eta(self):
-        return self._eta
+    eta=0.05
