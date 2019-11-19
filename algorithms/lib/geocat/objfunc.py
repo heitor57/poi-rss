18dfc3fcba4a2cb36e56f67409ba90f4b7e2cf4d
Tago Trotta <mrtrotta2010@gmail.com>
2019-11-19 17:31:54 -0300

    Implemented all methods

--- algorithms/lib/geocat/objfunc.py
+++ algorithms/lib/geocat/objfunc.py
@@ -3,31 +3,31 @@ import networkx as nx
 import lib.heuristics as heuristics
 
 def category_dis_sim(category1,category2,undirected_category_tree):
-    dissim=0.0
-    spd=nx.shortest_path_length(undirected_category_tree,category1,category2)
-    sim = 1.0 / (1.0 + spd)
-    dissim=1.0-sim
-    return dissim
+	dissim=0.0
+	spd=nx.shortest_path_length(undirected_category_tree,category1,category2)
+	sim = 1.0 / (1.0 + spd)
+	dissim=1.0-sim
+	return dissim
 
 def min_dist_to_list_cat(poi_id,pois,poi_cats,undirected_category_tree):
-    num_pois=len(pois)
-    min_dissim=1.0
-    cats_of_poi=poi_cats[poi_id]
-    if num_pois==0:
-        min_dissim=1.0
-    else:
-        for index_1 in pois:
+	num_pois=len(pois)
+	min_dissim=1.0
+	cats_of_poi=poi_cats[poi_id]
+	if num_pois==0:
+		min_dissim=1.0
+	else:
+		for index_1 in pois:
 
-            local_min_distance=1
-            cur_distance=0
-            for category1 in poi_cats[index_1]:
-                for category2 in cats_of_poi:
-                    cur_distance=category_dis_sim(
-                        category1,
-                        category2,undirected_category_tree)
-                    local_min_distance=min(local_min_distance,cur_distance)
-            min_dissim=min(min_dissim,local_min_distance)
-    return min_dissim
+			local_min_distance=1
+			cur_distance=0
+			for category1 in poi_cats[index_1]:
+				for category2 in cats_of_poi:
+					cur_distance=category_dis_sim(
+						category1,
+						category2,undirected_category_tree)
+					local_min_distance=min(local_min_distance,cur_distance)
+			min_dissim=min(min_dissim,local_min_distance)
+	return min_dissim
 
 def ld_objective_function(score,poi_id,pois,poi_cats,undirected_category_tree,div_weight):
     div=min_dist_to_list_cat(poi_id,pois,poi_cats,undirected_category_tree)
@@ -63,19 +63,19 @@ def ld(uid,training_matrix,tmp_rec_list,tmp_score_list,
     
 def gc(poi_id,rec_list,relevant_cats,poi_cats):
 
-    cats=set(poi_cats[poi_id])
-    for lid in rec_list:
-        cats.update(poi_cats[lid])
-    count_equal=0
-    for cat1 in relevant_cats:
-        for cat2 in cats:
-            if cat1 == cat2:
-                #print(cat1)
-                count_equal=count_equal+1    
-    return count_equal/len(relevant_cats)
+	cats=set(poi_cats[poi_id])
+	for lid in rec_list:
+		cats.update(poi_cats[lid])
+	count_equal=0
+	for cat1 in relevant_cats:
+		for cat2 in cats:
+			if cat1 == cat2:
+				#print(cat1)
+				count_equal=count_equal+1	
+	return count_equal/len(relevant_cats)
 
 def gc_list(rec_list,relevant_cats,poi_cats):
-    gc(rec_list[-1],rec_list[:-1],relevant_cats,poi_cats)
+	gc(rec_list[-1],rec_list[:-1],relevant_cats,poi_cats)
 
 
 def update_geo_cov(poi_id,log_poi_ids,rec_list_size,poi_cover,poi_neighbors,neighbors):
@@ -114,6 +114,7 @@ def update_geo_cov(poi_id,log_poi_ids,rec_list_size,poi_cover,poi_neighbors,neig
     PR=1-DP/(DP_IDEAL)
     
     return PR
+    
 def geodiv_objective_function(poi_id,log_poi_ids,rec_list_size,poi_cover,poi_neighbors,neighbors,current_proportionality,div_weight,score):
     pr=update_geo_cov(poi_id,log_poi_ids,rec_list_size,poi_cover,poi_neighbors,neighbors)
     div=max(0,pr-current_proportionality)
@@ -172,8 +173,6 @@ def persongeocat_objective_function(poi_id,score,
     objective_value=NORM_ILD_GC_PR(score,ild_div,gc_div,pr,current_proportionality,rec_list_size,div_geo_cat_weight,div_weight)
     return objective_value
 
-
-
 def geodiv(uid,training_matrix,tmp_rec_list,tmp_score_list,
             poi_neighbors,K,div_weight):
     
@@ -227,7 +226,7 @@ def geodiv(uid,training_matrix,tmp_rec_list,tmp_score_list,
     return rec_list,final_scores
 def geocat(uid,training_matrix,tmp_rec_list,tmp_score_list,
           poi_cats,poi_neighbors,K,undirected_category_tree,
-          div_geo_cat_weight,div_weight):
+          div_geo_cat_weight,div_weight,method='local_max'):
     range_K=range(K)
     rec_list=[]
 
@@ -268,8 +267,24 @@ def geocat(uid,training_matrix,tmp_rec_list,tmp_score_list,
                     neighbors.append(i)
         log_neighbors[poi_id]=neighbors
 
-    rec_list,final_scores = heuristics.local_max(tmp_rec_list, tmp_score_list, poi_cats, poi_neighbors, K, undirected_category_tree,
-				rec_list, relevant_cats, log_poi_ids, log_neighbors, poi_cover, current_proportionality,
-				div_geo_cat_weight, div_weight, final_scores)
-    
-    return rec_list,final_scores
+	rec_list = final_scores = None
+
+	if method == 'local_max':
+		rec_list,final_scores = heuristics.local_max(tmp_rec_list, tmp_score_list, poi_cats, poi_neighbors,
+				K, undirected_category_tree, rec_list, relevant_cats, log_poi_ids, log_neighbors, poi_cover,
+				current_proportionality, div_geo_cat_weight, div_weight, final_scores)
+
+	elif method == 'tabu_search':
+		rec_list,final_scores = heuristics.tabu_search(tmp_rec_list, tmp_score_list, poi_cats,
+				poi_neighbors, K, undirected_category_tree, relevant_cats, div_geo_cat_weight,
+				div_weight, user_log)
+
+	elif method == 'particle_swarm':
+		rec_list,final_scores = heuristics.particle_swarm(tmp_rec_list, tmp_score_list, poi_cats,
+				poi_neighbors, K, undirected_category_tree, relevant_cats, div_geo_cat_weight,
+				div_weight, user_log)
+
+	else:
+		print('Warning! Invalid method choice:', method)
+	
+	return rec_list,final_scores
