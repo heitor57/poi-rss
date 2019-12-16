00b401f228405af603138ed9e9f45d28f9476189
heitor57 <heitorwerneck@hotmail.com>
2019-12-16 07:14:53 +0000

    pathos parallel

---
+++ algorithms/lib/parallel_util.py
@@ -0,0 +1,35 @@
+import time
+from concurrent.futures import ProcessPoolExecutor
+import sys
+
+from tqdm import tqdm
+from pathos.multiprocessing import ProcessingPool as Pool
+
+def run_parallel(func, args,chunksize = 10):
+    # executor = ProcessPoolExecutor()
+    # num_args = len(args)
+    # results = [i for i in tqdm(executor.map(func,*list(zip(*args)),chunksize=chunksize),total=num_args)]
+    pool = Pool()
+    num_args = len(args)
+    results = pool.amap(func,*list(zip(*args)),chunksize=chunksize)
+    tmax = results._number_left
+    with tqdm(total=tmax) as pbar:
+        while not results.ready():
+            pbar.n = tmax - results._number_left
+            pbar.refresh()
+            time.sleep(1)
+    # results = [i for i in tqdm(pool.map(func,*list(zip(*args)),chunksize=chunksize),total=num_args)]
+    results = results.get()
+    return results
+
+
+
+# def run_parallel_rr(obj, func, args, chunksize = 10):
+#     input("waiting enter")
+#     with multiprocessing.Manager() as manager:
+#         executor = ProcessPoolExecutor()
+#         for i in range(len(args)):
+#             args[i] = (obj,) + args[i]
+#         num_args = len(args)
+#         results = [i for i in tqdm(executor.map(func,*list(zip(*args)),chunksize=chunksize),total=num_args)]
+#         return results
