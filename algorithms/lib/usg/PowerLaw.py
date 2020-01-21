fixed underflow

algorithms/lib/usg/PowerLaw.py
@@ -81,4 +81,5 @@ class PowerLaw(object):
 
     def predict(self, uid, lj):
         lj = self.poi_coos[lj]
-        return np.prod([self.pr_d(dist(self.poi_coos[li], lj)) for li in self.visited_lids[uid]])
+        res = [self.pr_d(dist(self.poi_coos[li], lj)) for li in self.visited_lids[uid]]
+        return np.prod(res,dtype=np.float128)