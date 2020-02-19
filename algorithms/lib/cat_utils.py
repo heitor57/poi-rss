0ba122d28fbe73c931f3c49be2e62adadb563aff
heitor57 <heitorwerneck@hotmail.com>
2020-02-19 05:58:18 +0000

    igraph

--- algorithms/lib/cat_utils.py
+++ algorithms/lib/cat_utils.py
@@ -2,6 +2,7 @@ import pandas as pd
 import networkx as nx
 import collections
 import numpy as np
+from igraph import *
 
 def get_most_detailed_categories(categories,dict_alias_title,dict_alias_depth):
     max_height=0
@@ -14,6 +15,36 @@ def get_most_detailed_categories(categories,dict_alias_title,dict_alias_depth):
             new_categories.append(category)
     return new_categories
 
+
+def cat_structs_igraph(catfilename)
+    df_categories=pd.read_json(catfilename)
+
+    dict_alias_title=dict()
+    for index, row in df_categories.iterrows():
+        dict_alias_title[row['alias']]=row['title']
+        dict_alias_title[row['title']]=row['alias']
+
+
+    category_tree = Graph(directed=False)
+    category_tree.add_vertices(list(df_categories['alias']))
+    category_tree.add_vertex('root')
+
+    for index, row in df_categories.iterrows():
+        if not row['parents']:
+            category_tree.add_edge(row['alias'],'root') # root node if no parents
+        else:
+            for parent_label in row['parents']:
+                category_tree.add_edge(row['alias'],parent_label)
+
+    undirected_category_tree=category_tree.to_undirected()
+    # dict alias depth
+    dict_alias_depth=dict()
+    for index, row in df_categories.iterrows():
+        dict_alias_depth[row['alias']]=category_tree.shortest_paths(category_tree,row['alias'],'root')[0][0]
+
+    df_categories=None
+
+    return dict_alias_title,category_tree,dict_alias_depth
 
 def cat_structs(catfilename):
     df_categories=pd.read_json(catfilename)#"../data/categories.json"
